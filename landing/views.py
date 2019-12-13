c385e90dc971cf9a1df008d47cd61e29e3ad8c9f
Leonardo <leomail2479@gmail.com>
2019-12-12 21:29:01 -0300

    revisão

--- landing/views.py
+++ landing/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import Aluno, Usuario
 
 # Create your views here.
+def home(request):
+    return render (request, 'home.html') 
 
 def index(request):
     if request.method == 'POST':
@@ -14,7 +16,7 @@ def index(request):
         data_aluno.nome = request.POST['nome']
         data_aluno.frase = request.POST['frase']
         data_aluno.save()
-        
+       
     return render(request, 'index.html')
 
 def listar(request):
@@ -30,4 +32,12 @@ def deletar(request):
         item.ativo = False
         item.save()
         return redirect('aluno/listar')
-    return render(request, 'lista.html') 
+    return render(request, 'lista.html')
+
+def login(request):
+    if request.method == 'POST':
+        data_usuario = Usuario()
+        data_usuario.email = request.POST['email']
+        data_usuario.senha = request.POST['senha']
+        data_usuario.save()
+    return render(request, 'login.html')    
